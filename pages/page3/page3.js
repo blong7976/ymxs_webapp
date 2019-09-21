e90aef98a4cba9552f128537e3b57cc2b5f97ab2
blong7976 <2456399912@qq.com>
2019-09-21 12:26:42 +0800

    Signed-off-by: blong7976 <2456399912@qq.com>

--- pages/page3/page3.js
+++ pages/page3/page3.js
@@ -5,7 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    navlist: [
+      {
+        name: "美食天地",
+        icon: "../../images/shop-icon1.png",
+        path: ""
+      },
+      {
+        name: "丽人美发",
+        icon: "../../images/shop-icon2.png",
+        path: ""
+      },
+      {
+        name: "生活服务",
+        icon: "../../images/shop-icon3.png",
+        path: ""
+      },
+      {
+        name: "建材家居",
+        icon: "../../images/shop-icon4.png",
+        path: ""
+      },
+      {
+        name: "婚庆摄影",
+        icon: "../../images/shop-icon5.png",
+        path: ""
+      },
+      {
+        name: "休闲娱乐",
+        icon: "../../images/shop-icon6.png",
+        path: ""
+      },
+      {
+        name: "教育培训",
+        icon: "../../images/shop-icon7.png",
+        path: ""
+      },
+      {
+        name: "购物天地",
+        icon: "../../images/shop-icon8.png",
+        path: ""
+      },
+      {
+        name: "教育培训",
+        icon: "../../images/shop-icon9.png",
+        path: ""
+      },
+      {
+        name: "购物天地",
+        icon: "../../images/shop-icon10.png",
+        path: ""
+      }
+    ],
   },
 
   /**
